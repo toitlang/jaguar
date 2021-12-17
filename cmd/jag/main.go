977cfc6dfb39cc67439ef34e16a4c77d995ddbe3
Kasper Lund <kasper@toit.io>
2021-12-17 16:54:40 +0100

    Prepare to releaes v0.2.1

--- cmd/jag/main.go
+++ cmd/jag/main.go
@@ -7,8 +7,8 @@ package main
 import "github.com/toitlang/jaguar/cmd/jag/commands"
 
 var (
-	date       = "2021-12-17T15:21:43Z"
-	version    = "v0.2.0"
+	date       = "2021-12-17T15:54:14Z"
+	version    = "v0.2.1"
 	sdkVersion = "v0.10.4"
 )
 
