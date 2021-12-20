8a533694d6e7588f12f318098ec303b94da25e11
Jesper Lindstrøm <jesper@toitware.com>
2021-12-20 13:14:53 +0100

    Use toitpkg as libaries

--- cmd/jag/commands/pkg.go
+++ cmd/jag/commands/pkg.go
@@ -5,33 +5,23 @@
 package commands
 
 import (
-	"os"
+	"context"
 
 	"github.com/spf13/cobra"
+	"github.com/toitlang/tpkg/commands"
+	"github.com/toitlang/tpkg/config/store"
+	"github.com/toitlang/tpkg/pkg/tracking"
 )
 
-func PkgCmd() *cobra.Command {
-	cmd := &cobra.Command{
-		Use:          "pkg",
-		Short:        "Manage your Toit packages",
-		Long:         "Manage your Toit packages.\n\nUse 'jag pkg -- --help' for detailed help.",
-		SilenceUsage: true,
-		RunE: func(cmd *cobra.Command, args []string) error {
-			ctx := cmd.Context()
-			sdk, err := GetSDK()
-			if err != nil {
-				return err
-			}
-
-			toitPkg := sdk.ToitPkg(ctx, append([]string{"pkg"}, args...))
-			toitPkg.Stdin = os.Stdin
-			toitPkg.Stdout = os.Stdout
-			toitPkg.Stderr = os.Stderr
-			if err := toitPkg.Run(); err != nil {
-				return err
-			}
-			return nil
-		},
+func PkgCmd(info Info) *cobra.Command {
+	track := func(ctx context.Context, te *tracking.Event) error {
+		return nil
 	}
-	return cmd
+
+	s := store.NewViper("", info.SDKVersion, false, false)
+	pkg, err := commands.Pkg(commands.DefaultRunWrapper, track, s, nil)
+	if err != nil {
+		panic(err)
+	}
+	return pkg
 }
