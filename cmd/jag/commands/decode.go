ac2e500fb6379544a5717550cbf60fca08bd7bf3
Erik Corry <erik@toit.io>
2022-06-23 12:18:42 +0200

    Automatically call jag decode from jag monitor

--- cmd/jag/commands/decode.go
+++ cmd/jag/commands/decode.go
@@ -27,72 +27,75 @@ func DecodeCmd() *cobra.Command {
 		Args:         cobra.ExactArgs(1),
 		SilenceUsage: true,
 		RunE: func(cmd *cobra.Command, args []string) error {
-			ctx := cmd.Context()
-			sdk, err := GetSDK(ctx)
-			if err != nil {
-				return err
-			}
-
-			base64Message := args[0]
-			message, err := base64.StdEncoding.DecodeString(base64Message)
-			if err != nil {
-				return err
-			}
-
-			var decoded []interface{}
-			if err = ubjson.Unmarshal(message, &decoded); err != nil {
-				return fmt.Errorf("failed to parse message as ubjson, reason: %v", err)
-			}
-
-			if len(decoded) != 4 && len(decoded) != 5 {
-				return fmt.Errorf("message did not have correct format")
-			}
-
-			i := 0
-			if v, ok := decoded[i].(int64); !ok || rune(v) != 'X' {
-				return fmt.Errorf("message did not have correct format")
-			}
-			i++
-
-			_, ok := decoded[i].(string)
-			if !ok {
-				return fmt.Errorf("message did not have correct format")
-			}
-			i++
-
-			if len(decoded) == 5 {
-				if _, ok := decoded[i].(string); !ok {
-					return fmt.Errorf("message did not have correct format")
-				}
-				i++
-			}
-
-			var programIdBytes []byte
-			if mapstructure.Decode(decoded[i], &programIdBytes) != nil {
-				return fmt.Errorf("message did not have correct format")
-			}
-
-			programId, err := uuid.FromBytes(programIdBytes)
-			if err != nil {
-				return fmt.Errorf("failed to parse program id: %v", err)
-			}
-
-			snapshotsCache, err := directory.GetSnapshotsCachePath()
-			if err != nil {
-				return err
-			}
-			snapshot := filepath.Join(snapshotsCache, programId.String()+".snapshot")
-
-			if _, err := os.Stat(snapshot); errors.Is(err, os.ErrNotExist) {
-				fmt.Fprintf(os.Stderr, "No such file: %s\n", snapshot)
-				return fmt.Errorf("cannot find snapshot for program: %s", programId.String())
-			}
-
-			decodeCmd := sdk.ToitRun(ctx, sdk.SystemMessageSnapshotPath(), snapshot, "-b", base64Message)
-			decodeCmd.Stderr = os.Stderr
-			decodeCmd.Stdout = os.Stdout
-			return decodeCmd.Run()
+			return jagDecode(cmd, args[0])
 		},
 	}
 	return cmd
+}
+
+func jagDecode(cmd *cobra.Command, base64Message string) error {
+	ctx := cmd.Context()
+	sdk, err := GetSDK(ctx)
+	if err != nil {
+		return err
+	}
+
+	message, err := base64.StdEncoding.DecodeString(base64Message)
+	if err != nil {
+		return err
+	}
+
+	var decoded []interface{}
+	if err = ubjson.Unmarshal(message, &decoded); err != nil {
+		return fmt.Errorf("failed to parse message as ubjson, reason: %v", err)
+	}
+
+	if len(decoded) != 4 && len(decoded) != 5 {
+		return fmt.Errorf("message did not have correct format")
+	}
+
+	i := 0
+	if v, ok := decoded[i].(int64); !ok || rune(v) != 'X' {
+		return fmt.Errorf("message did not have correct format")
+	}
+	i++
+
+	_, ok := decoded[i].(string)
+	if !ok {
+		return fmt.Errorf("message did not have correct format")
+	}
+	i++
+
+	if len(decoded) == 5 {
+		if _, ok := decoded[i].(string); !ok {
+			return fmt.Errorf("message did not have correct format")
+		}
+		i++
+	}
+
+	var programIdBytes []byte
+	if mapstructure.Decode(decoded[i], &programIdBytes) != nil {
+		return fmt.Errorf("message did not have correct format")
+	}
+
+	programId, err := uuid.FromBytes(programIdBytes)
+	if err != nil {
+		return fmt.Errorf("failed to parse program id: %v", err)
+	}
+
+	snapshotsCache, err := directory.GetSnapshotsCachePath()
+	if err != nil {
+		return err
+	}
+	snapshot := filepath.Join(snapshotsCache, programId.String()+".snapshot")
+
+	if _, err := os.Stat(snapshot); errors.Is(err, os.ErrNotExist) {
+		fmt.Fprintf(os.Stderr, "No such file: %s\n", snapshot)
+		return fmt.Errorf("cannot find snapshot for program: %s", programId.String())
+	}
+
+	decodeCmd := sdk.ToitRun(ctx, sdk.SystemMessageSnapshotPath(), snapshot, "-b", base64Message)
+	decodeCmd.Stderr = os.Stderr
+	decodeCmd.Stdout = os.Stdout
+	return decodeCmd.Run()
 }
