bf44e18b3ffd934c6b5d1713ad326929ad1b57e9
Florian Loitsch <florian@toit.io>
2023-01-16 11:46:10 +0100

    Build ARM executables. (#305)

--- cmd/jag/commands/setup.go
+++ cmd/jag/commands/setup.go
@@ -19,12 +19,17 @@ import (
 	"github.com/toitlang/jaguar/cmd/jag/directory"
 )
 
-func getToitSDKURL(version string) string {
+func getToitSDKURL(version string) (string, error) {
 	currOS := runtime.GOOS
 	if currOS == "darwin" {
 		currOS = "macos"
 	}
-	return fmt.Sprintf("https://github.com/toitlang/toit/releases/download/%s/toit-%s.tar.gz", version, currOS)
+	currARCH := runtime.GOARCH
+	if currARCH == "arm" || currARCH == "arm64" {
+		fmt.Printf("Enter the URL of the ARM SDK (version %v).", version)
+		return ReadLine()
+	}
+	return fmt.Sprintf("https://github.com/toitlang/toit/releases/download/%s/toit-%s.tar.gz", version, currOS), nil
 }
 
 func getAssetsURL(version string) string {
@@ -221,9 +226,12 @@ func downloadSdk(ctx context.Context, version string) error {
 }
 
 func downloadSdkTo(ctx context.Context, version string, sdkPath string) error {
-	sdkURL := getToitSDKURL(version)
+	sdkURL, err := getToitSDKURL(version)
+	if err != nil {
+		return err
+	}
 	fmt.Printf("Downloading Toit SDK from %s ...\n", sdkURL)
-	sdk, err := download(ctx, getToitSDKURL(version))
+	sdk, err := download(ctx, sdkURL)
 	if err != nil {
 		return err
 	}
