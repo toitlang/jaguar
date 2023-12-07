a4de14b535c5396886274a5bb5a77524c861f658
Florian Loitsch <florian@toit.io>
2023-12-07 12:11:59 +0100

    Update tools/windows_installer/build.bat
    Co-authored-by: Kasper Lund <kasper@toit.io>

--- tools/windows_installer/build.bat
+++ tools/windows_installer/build.bat
@@ -4,7 +4,7 @@ set BUILD_DIRECTORY=%3
 
 set INSTALLER_NAME="%cd%\tools\windows_installer\jag_installer.exe"
 
-set inno=ISCC.exe
+set INNO=ISCC.exe
 
-%inno% /Qp /dMyAppVersion=%VERSION% /dMyAppExeName="%BIN_PATH%" "%cd%/tools/windows_installer/installer.iss"
+%INNO% /Qp /dMyAppVersion=%VERSION% /dMyAppExeName="%BIN_PATH%" "%cd%/tools/windows_installer/installer.iss"
 move %INSTALLER_NAME% %BUILD_DIRECTORY%
