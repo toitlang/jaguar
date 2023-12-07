32627c5659a8f7c62fa642f029e508c837b0d840
Florian Loitsch <florian@toit.io>
2023-12-07 14:28:50 +0400

    Fix Windows signing.

--- tools/windows_installer/build.bat
+++ tools/windows_installer/build.bat
@@ -4,7 +4,7 @@ set BUILD_DIRECTORY=%3
 
 set INSTALLER_NAME="%cd%\tools\windows_installer\jag_installer.exe"
 
-set inno="C:\Program Files (x86)\Inno Setup 6\ISCC.exe"
+set inno=ISCC.exe
 
 %inno% /Qp /dMyAppVersion=%VERSION% /dMyAppExeName="%BIN_PATH%" "%cd%/tools/windows_installer/installer.iss"
 move %INSTALLER_NAME% %BUILD_DIRECTORY%
