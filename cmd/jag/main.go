f6fcbb13629984bebd22179f7911c836d0ab063a
Florian Loitsch <florian@toit.io>
2026-03-01 20:14:39 +0100

    Prepare release. (#678)

--- cmd/jag/main.go
+++ cmd/jag/main.go
@@ -14,8 +14,8 @@ import (
 
 var (
 	// When updating the version here, also update it in debian/changelog.
-	version    = "v1.60.0"
-	sdkVersion = "v2.0.0-alpha.189"
+	version    = "v1.61.0"
+	sdkVersion = "v2.0.0-alpha.190"
 )
 
 var buildDate = "unknown"
