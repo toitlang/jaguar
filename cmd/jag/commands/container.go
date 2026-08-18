d911da2ac16dd57493e60c0c7c9180976f6ddde8
Florian Loitsch <florian@loitsch.com>
2026-08-18 19:18:43 +0200

    Simplify proxied container timeout help

--- cmd/jag/commands/container.go
+++ cmd/jag/commands/container.go
@@ -84,9 +84,8 @@ func ContainerInstallCmd() *cobra.Command {
 			"     Disables the HTTP server on the device.\n" +
 			"	'-D jag.timeout': Set the timeout for Jaguar to wait for the program to\n" +
 			"     finish. The value can be a number of seconds or a duration string.\n" +
-			"     If jag.disabled is enabled, then the default is 10 seconds. When\n" +
-			"     installing through a UART proxy, no timeout applies to the container's\n" +
-			"     initial run. A configured timeout is retained for later starts.\n" +
+			"     If jag.disabled is enabled, then the default is 10 seconds. Timeouts do\n" +
+			"     not apply to the initial run when installing through a UART proxy.\n" +
 			"	'-D jag.interval' (or --interval):Interval for container starts\n" +
 			"     (e.g., '30s', '5m', '1h'). When specified, Jaguar will start the\n" +
 			"     container at the specified interval if it has previously exited.",
